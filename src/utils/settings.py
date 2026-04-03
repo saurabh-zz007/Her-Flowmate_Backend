02a31de449ef554b05d588b6effabbb08d6464ae
Saurabh Vishwakarma <saurabh.vishw.007@gmail.com>
2026-04-03 08:49:30 +0530

    updated client id

--- src/utils/settings.py
+++ src/utils/settings.py
@@ -4,6 +4,7 @@ class Settings(BaseSettings):
     model_config=SettingsConfigDict(env_file=".env", extra="ignore")
 
     GOOGLE_CLOUD_CLIENT_ID: str
+    GOOGLE_CLOUD_CLIENT_ID_2: str
     DB_CONNECTION: str
     ALGORITHM: str
     SECRET_KEY: str
