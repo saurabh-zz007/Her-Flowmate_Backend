b27a652b16f88d1ef9e025beafe5445246485e45
Saurabh Vishwakarma <saurabh.vishw.007@gmail.com>
2026-04-10 14:39:01 +0530

    Added Period :-<

--- src/logs/router.py
+++ src/logs/router.py
@@ -1,20 +1,26 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from src.logs import controllers
-
+from sqlalchemy.orm import Session
+from src.utils.db import get_db
+from src.utils.helpers import get_user_id
+import uuid
+from src.logs.models import PeriodModel
+from datetime import date
+from src.logs.data_transfer_objects import PeriodSchema
 
 logs_router = APIRouter(prefix="/logs")
 
-@logs_router.get("/periods")
-def get_periods():
-    return controllers.get_periods()
+@logs_router.get("/periods", response_model=list[PeriodSchema])
+def get_periods(db: Session = Depends(get_db), user_id: uuid.UUID = Depends(get_user_id)):
+    return controllers.get_periods(db, user_id)
 
-@logs_router.post("/periods")
-def create_periods():
-    return controllers.create_periods()
+@logs_router.post("/periods", response_model=PeriodSchema)
+def create_periods(data: PeriodSchema, db:Session = Depends(get_db), user_id: uuid.UUID = Depends(get_user_id)):
+    return controllers.create_periods(data, db, user_id)
 
-@logs_router.delete("/periods")
-def delete_periods():
-    return controllers.delete_periods()
+@logs_router.delete("/periods", response_model=PeriodSchema)
+def delete_periods(start_date: date, db: Session = Depends(get_db), user_id: uuid.UUID = Depends(get_user_id)):
+    return controllers.delete_periods(start_date, db, user_id)
 
 @logs_router.post("/daily")
 def update_daily_log():
