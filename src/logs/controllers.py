55f026a99161b05daaeb3ea61196c7f4cce7a4a3
Saurabh Vishwakarma <saurabh.vishw.007@gmail.com>
2026-04-11 11:50:12 +0530

    All endpoints are ready

--- src/logs/controllers.py
+++ src/logs/controllers.py
@@ -1,9 +1,10 @@
 import uuid
+from sqlalchemy import Column
 from sqlalchemy.orm import Session
-from src.logs.models import PeriodModel
+from src.logs.models import PeriodModel, DailyLogModel
 from typing import List
-from datetime import date
-from src.logs.data_transfer_objects import PeriodSchema
+from datetime import date, timedelta
+from src.logs.data_transfer_objects import PeriodSchema, DailyLogSchema
 
 def get_periods(db:Session, user_id: uuid.UUID):
     logs = db.query(PeriodModel).filter(PeriodModel.user_id == user_id).order_by(PeriodModel.start_date).all()
@@ -49,11 +50,45 @@ def delete_periods(start_date: date, db: Session, user_id: uuid.UUID):
         db.rollback()
         raise e
 
-def update_daily_log():
-    pass
+def update_daily_log(data: DailyLogSchema, db: Session, user_id: uuid.UUID):
+    log = db.query(DailyLogModel).filter(DailyLogModel.user_id == user_id, DailyLogModel.log_date == data.log_date).first()
 
-def get_daily_log():
-    pass
+    if not log:
+        log = DailyLogModel(
+            user_id = user_id,
+            log_date = data.log_date,
+            moods = data.moods,
+            symptoms = data.symptoms,
+            water_intake = data.water_intake,
+            flow_intensity = data.flow_intensity,
+            physical_activity = data.physical_activity,
+            notes = data.notes
+        )
+    
+    else:
+        for field, value in data.model_dump().items():
+            if value is not None:
+                setattr(log, field, value)
 
-def trends():
-    pass
+    db.add(log)
+    db.commit()
+    db.refresh(log)
+
+    return log
+
+def get_daily_log(log_date: date, db:Session, user_id: uuid.UUID):
+    log = db.query(DailyLogModel).filter(DailyLogModel.user_id == user_id, DailyLogModel.log_date == log_date).first()
+    if not log:
+        raise Exception("Daily log not found")
+    return log
+
+def trends(range : int, db: Session, user_id: uuid.UUID):
+    end_date = date.today()
+    start_date = end_date - timedelta(days=range)
+
+    logs = db.query(DailyLogModel).filter(DailyLogModel.user_id == user_id, DailyLogModel.log_date >= start_date, DailyLogModel.log_date <= end_date).all()
+
+    if not logs:
+        raise Exception("No logs found for the specified range")
+    
+    return logs
